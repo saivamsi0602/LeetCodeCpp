d29561986769f990401860944b8092f1b0e59724
Sai vamsi Andiboyina <sandiboyina@adobe.com>
2024-03-17 15:08:05 +0530

    update bit tree

--- DataStructuresAndAlgorithms/DataStructures/Trees/include/FenwickTree.h
+++ DataStructuresAndAlgorithms/DataStructures/Trees/include/FenwickTree.h
@@ -6,14 +6,33 @@ namespace DataStructures
         class FenwickTree
         {
         private:
-            std::vector<int> arr,nums;
-            void updateTillRoot(int index, int diff);
-            int getPointQuery(int index);
+            /* data */
+            std::vector<int> arr, tree;
+            void updateTree(int index, int diff){
+                for(int i=index;i<tree.size();i+=(i&-i))
+                    tree[i]+=diff;
+            }
+            int pointQuery(int index){
+                int res = 0;
+                for(int i=index;i>0;i-=(i&-i)){
+                    res+=tree[i];
+                }
+                return res;
+            }
         public:
-            FenwickTree(std::vector<int>& nums);
-            ~FenwickTree();
-            int getSum(int l, int r);
-            void update(int index, int val);
+            FenwickTree(const std::vector<int>& nums): arr(nums){
+                int n = arr.size();
+                tree.resize(n+1,0);
+                for(int i=0;i<n;i++)
+                    updateTree(i+1,arr[i]);
+            }
+            void update(int index, int val){
+                updateTree(index+1,val-arr[index]);
+                arr[index]=val;
+            }
+            int rangeQuery(int left, int right){
+                return pointQuery(right+1)-pointQuery(left);
+            }
         };
     } // namespace Trees
 } // namespace DataStructures
